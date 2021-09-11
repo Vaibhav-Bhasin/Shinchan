e41268c9087cfaf58d44fdf121ea2b08c5bc611c
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-11 22:03:19 +0530

    Update Shinchan.py

--- Shinchan.py
+++ Shinchan.py
@@ -1,1 +1,13 @@
 
+""" Vabhav Bhasin"""
+
+from turtle import *
+
+# Functions for drawing Shinchan
+
+# Definition the position of the pen
+def myPosition(x, y):
+    penup()
+    goto(x, y)
+    pendown()
+
