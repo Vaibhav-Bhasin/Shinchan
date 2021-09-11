88aa3911468cc4ffbfa782f4675b1647952891e9
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-09-11 22:10:55 +0530

    Update Shinchan.py

--- Shinchan.py
+++ Shinchan.py
@@ -407,3 +407,17 @@ def leftHand2():
         for i in range(2): 
             circle(4,90)
     end_fill()
+
+
+# Mouth function
+def myMouth():
+    myPosition(-25,200)
+    left(65)
+    fillcolor('#77332e')
+    begin_fill()
+    #circle(20)
+    #forward(20)
+    for i in range(2): 
+        circle(25,90) 
+        circle(25//2,90)
+    end_fill()
